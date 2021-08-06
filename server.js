0eb2f72dbdbf3da155a97fe1c8f7164c7b92f6ce
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-08-06 13:28:26 -0500

    created routes and set up methods on pages

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Import express
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 // Array with Pokemon data to use
 const pokemon = [
@@ -34,6 +36,9 @@ const pokemon = [
   },
 ];
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
 // GET route to display index page
 app.get("/pokemon", (req, res) => {
   res.render("index.ejs", {
@@ -41,11 +46,42 @@ app.get("/pokemon", (req, res) => {
   });
 });
 
+// GET route to create new object
+app.get("/pokemon/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 // GET route to display indexed data
 app.get("/pokemon/:index", (req, res) => {
   res.render("show.ejs", {
     pokemon: pokemon[req.params.index],
   });
+});
+
+// GET route to edit an existing Pokemon
+app.get("/pokemon/:index/edit", function (req, res) {
+  res.render("edit.ejs", {
+    pokemon: pokemon[req.params.index],
+    index: req.params.index,
+  });
+});
+
+// PUT route to update an existing Pokemon
+app.put("/pokemon/:index", (req, res) => {
+  pokemon[req.params.index] = req.body;
+  res.redirect("/pokemon");
+});
+
+// POST route to add new Pokemon
+app.post("/pokemon", (req, res) => {
+  pokemon.push(req.body);
+  res.redirect("/pokemon");
+});
+
+// DELETE route to delete a Pokemon
+app.delete("/pokemon/:index", (req, res) => {
+  pokemon.splice(req.params.index, 1);
+  res.redirect("/pokemon");
 });
 
 // Server listen on port 3000
