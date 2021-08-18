d79e07f1b1d63010a7e012f8a35a5257b39d4ba0
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-08-18 12:42:12 -0500

    Corrected model associations

--- models/users.js
+++ models/users.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsTo(models.Team, { foreignKey: "teamId" });
+      Users.belongsTo(models.Team, { foreignKey: "teamId" });
     }
   }
   Users.init(
